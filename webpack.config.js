05c762020c93341807de65ca9520826f593d4b70
yoshida <yoshida122121@gmail.com>
2024-08-07 18:44:18 +0800

    chore: Update webpack configuration to use CommonJS modules and webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-import path from 'path';
+const path = require('path');
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import CopyPlugin from 'copy-webpack-plugin';
 import webpack from 'webpack';
